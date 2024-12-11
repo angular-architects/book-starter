145d0cfadc723bcc3a82de6d267c833ba54614fe
Manfred Steyer <manfred.steyer@gmx.net>
2024-12-11 12:09:46 +0100

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@Component({
+    imports: [SidebarComponent, NavbarComponent],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Hello World!';
+}
